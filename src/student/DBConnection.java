88f61cd3d613999f89964527e96080fafcf38df8
Mansi Khairnar <mansikhairnar003@gmail.com>
2026-03-25 22:44:07 +0530

    Add files via upload

--- src/student/DBConnection.java
+++ src/student/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/studentdb";
         String user = "root"; // Change it if your username is different
-        String password = "m@n$!#3102002"; // Add your MySQL password if you have one
+        String password = "your_password"; // Add your MySQL password if you have one
 
         return DriverManager.getConnection(url, user, password);
     }
